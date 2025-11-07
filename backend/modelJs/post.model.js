c77d268c1b70ed97f60b1947f36ee4cebb56eb1e
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2025-11-07 22:46:25 +0530

    Remove user field from post model
    Removed user field from post schema.

--- backend/modelJs/post.model.js
+++ backend/modelJs/post.model.js
@@ -3,10 +3,7 @@ import mongoose from "mongoose";
 
 const postSchema = new mongoose.Schema({
 
-    user:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
+
     },
     text:{
         type: String,
