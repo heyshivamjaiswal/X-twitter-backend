e12df132a95f68de33165fddb89972f201515309
Shivam Jaiswal <153024426+heyshivamjaiswal@users.noreply.github.com>
2025-12-23 21:47:57 +0530

    Update connectMongodb.js

--- backend/db/connectMongodb.js
+++ backend/db/connectMongodb.js
@@ -1,14 +1,1 @@
-import mongoose from "mongoose";
-import dotenv from "dotenv";
 
-const connectMongo = async ()=>{
-    try{
-    const conn = await mongoose.connect(process.env.MONGO_URL)
-    console.log(`MongoDB connected: ${conn.connection.host}`)
-    }catch(error){
-      console.log(`Error connection to mongoDB: ${error.message}`);
-      process.exit(1);  
-    }
-}
-
-export default connectMongo;  
